0661226518c4d1b10b96651b0b28c7efa834af4c
9ziggy9 <davidarogers@protonmail.com>
2023-03-02 13:21:49 -0800

    introducing global vars

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,27 +1,26 @@
 import "../index.css";
 
-export default function Square({coord, piece, color, hl, setHl,
-                                selected, game, setGame, style}) {
+export default function Square({coord, piece, style, state}) {
   function handleClick(e) {
-    if (!selected.current.piece) {
-      if (!piece) return;
-      selected.current.piece = [coord, piece, color];
-      setHl(e.target);
-      return;
-    }
-    if (!selected.current.dest) {
-      selected.current.dest = [coord, piece, color];
-      setGame({
-        ...game,
-        [selected.current.piece[0]]: null,
-        [selected.current.dest[0]]:
-	  selected.current.piece[1]+"_"+selected.current.piece[2],
-      });
-      selected.current.piece = null;
-      selected.current.dest = null;
-      setHl(null);
-      return;
-    }
+    // if (!selected.current.piece) {
+    //   if (!piece) return;
+    //   selected.current.piece = [coord, piece, color];
+    //   setHl(e.target);
+    //   return;
+    // }
+    // if (!selected.current.dest) {
+    //   selected.current.dest = [coord, piece, color];
+    //   setGame({
+    //     ...game,
+    //     [selected.current.piece[0]]: null,
+    //     [selected.current.dest[0]]:
+    // 	  selected.current.piece[1]+"_"+selected.current.piece[2],
+    //   });
+    //   selected.current.piece = null;
+    //   selected.current.dest = null;
+    //   setHl(null);
+    //   return;
+    // }
   }
 
   return (
@@ -31,8 +30,8 @@ export default function Square({coord, piece, color, hl, setHl,
       key={coord}
       style={{
         ...style,
-	outline: (hl?.id === coord ? "solid 5px var(--my-blue)" : ""),
-	outlineOffset: (hl?.id === coord ? "-5px" : ""),
+	outline: (state?.hl?.id === coord ? "solid 5px var(--my-blue)" : ""),
+	outlineOffset: (state?.hl?.id === coord ? "-5px" : ""),
       }}
     ></div>
   );
