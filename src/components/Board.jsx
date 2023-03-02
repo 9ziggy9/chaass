73534e469779c45ee5c09df452cafa730e69c714
9ziggy9 <davidarogers@protonmail.com>
2023-03-01 17:00:55 -0800

    phase-two complete

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,5 +1,6 @@
 import "../index.css";
 import Square from "./Square";
+import {useEffect, useState} from "react";
 
 const COORDS = [
   ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
@@ -12,7 +13,38 @@ const COORDS = [
   ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
 ];
 
+const newGame = (state) => {
+  return {
+    ...state,
+    "a8": "rook_black", "b8": "knight_black", "c8": "bishop_black",
+    "d8": "queen_black", "e8": "king_black", "f8": "bishop_black",
+    "g8": "knight_black", "h8": "rook_black",
+    "a1": "rook_white", "b1": "knight_white", "c1": "bishop_white",
+    "d1": "queen_white", "e1": "king_white", "f1": "bishop_white",
+    "g1": "knight_white", "h1": "rook_white",
+    "a7": "pawn_black","b7": "pawn_black","c7": "pawn_black",
+    "d7": "pawn_black","e7": "pawn_black","g7": "pawn_black",
+    "f7": "pawn_black","h7": "pawn_black",
+    "a2": "pawn_white","b2": "pawn_white","c2": "pawn_white",
+    "d2": "pawn_white","e2": "pawn_white","g2": "pawn_white",
+    "f2": "pawn_white","h2": "pawn_white",
+  };
+};
+
 export default function Board() {
+  const [game, setGame] = useState(
+    // COMPUTED PROPERTY NAMES
+    COORDS.flat().reduce((acc, c) => ({...acc, [c]: null}), {})
+  );
+
+  useEffect(() => {
+    setGame(newGame(game));
+  }, []);
+
+  useEffect(() => {
+    console.log(game);
+  }, [game]);
+
   return (
     <div id="layer-board">
       {COORDS.map((row, x) => row.map((coord, y) => (
@@ -25,8 +57,19 @@ export default function Board() {
             ),
             width: "100%",
             height: "100%",
+            backgroundSize: "contain",
+            backgroundImage: (
+              game[coord]
+                ? `url(${require(`../img/${game[coord]}.png`)})`
+		: ""
+            )
           }}
-          coord={coord}/>
+          piece={game[coord]?.split("_")[0]}
+          color={game[coord]?.split("_")[1]}
+          key={coord}
+          coord={coord}
+          game={game}
+        />
       )))}
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,23 @@
 import "../index.css";
+import {useEffect, useState} from "react";
 
-export default function Square({coord, style}) {
+export default function Square({
+  coord, piece, color,
+  game, style
+}) {
+  const [current, setCurrent] = useState(null);
+
+  function handleClick() {
+    setCurrent([coord, piece, color]);
+  }
+
+  useEffect(() => {
+    console.log(current);
+  }, [current]);
+
   return (
     <div
+      onClick={handleClick}
       id={coord}
       key={coord}
       style={style}
