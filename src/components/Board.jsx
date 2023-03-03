2e250ced57cf9d99e55b3c3a326dce05cf0610fb
9ziggy9 <davidarogers@protonmail.com>
2023-03-02 17:12:13 -0800

    reducer refactoring complete

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,49 +1,13 @@
 import "../index.css";
 import Square from "./Square";
-import {useEffect, useState, useRef} from "react";
+import {useRef} from "react";
+import {COORDS} from "../global.js";
 
-const COORDS = [
-  ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
-  ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
-  ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
-  ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
-  ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
-  ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
-  ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
-  ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
-];
-
-const newGame = (state) => {
-  return {
-    ...state,
-    "a8": "rook_black", "b8": "knight_black", "c8": "bishop_black",
-    "d8": "queen_black", "e8": "king_black", "f8": "bishop_black",
-    "g8": "knight_black", "h8": "rook_black",
-    "a1": "rook_white", "b1": "knight_white", "c1": "bishop_white",
-    "d1": "queen_white", "e1": "king_white", "f1": "bishop_white",
-    "g1": "knight_white", "h1": "rook_white",
-    "a7": "pawn_black","b7": "pawn_black","c7": "pawn_black",
-    "d7": "pawn_black","e7": "pawn_black","g7": "pawn_black",
-    "f7": "pawn_black","h7": "pawn_black",
-    "a2": "pawn_white","b2": "pawn_white","c2": "pawn_white",
-    "d2": "pawn_white","e2": "pawn_white","g2": "pawn_white",
-    "f2": "pawn_white","h2": "pawn_white",
-  };
-};
-
-export default function Board() {
-  const [game, setGame] = useState(
-    COORDS.flat().reduce((acc, c) => ({...acc, [c]: null}), {})
-  );
-
-  useEffect(() => {
-    setGame(newGame(game));
-  }, []);
-
-  useEffect(() => {
-    console.log(game);
-  }, [game]);
-
+export default function Board({state, dispatch}) {
+  const selected = useRef({
+    piece: null,
+    dest: null
+  });
   return (
     <div id="layer-board">
       {COORDS.map((row, x) => row.map((coord, y) => (
@@ -58,15 +22,18 @@ export default function Board() {
             height: "100%",
             backgroundSize: "contain",
             backgroundImage: (
-              game[coord]
-                ? `url(${require(`../img/${game[coord]}.png`)})`
+              state.board[coord]
+                ? `url(${require(`../img/${state.board[coord]}.png`)})`
 		: ""
             )
           }}
-          piece={game[coord]?.split("_")[0]}
-          color={game[coord]?.split("_")[1]}
+          piece={state.board[coord]?.split("_")[0]}
+          color={state.board[coord]?.split("_")[1]}
           key={coord}
           coord={coord}
+          state={state}
+          dispatch={dispatch}
+          selected={selected}
         />
       )))}
     </div>
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,27 @@
 import "../index.css";
 
-export default function Square({coord, piece, style}) {
+export default function Square({coord, piece, style, color,
+                                state, dispatch, selected}) {
   function handleClick(e) {
-    console.log("Hello, world!");
+    if (!selected.current.piece) {
+      if (!piece) return;
+      selected.current.piece = [coord, piece, color];
+      dispatch({type: "setHighlight", payload: e.target});
+      return;
+    }
+    if (!selected.current.dest) {
+      selected.current.dest = [coord, piece, color];
+      dispatch({type: "makeMove", payload: {
+        piece: selected.current.piece[1],
+        color: selected.current.piece[2],
+        pCoord: selected.current.piece[0],
+        dCoord: selected.current.dest[0]
+      }});
+      selected.current.piece = null;
+      selected.current.dest = null;
+      dispatch({type: "setHighlight", payload: e.target});
+      return;
+    }
   }
 
   return (
@@ -10,7 +29,11 @@ export default function Square({coord, piece, style}) {
       onClick={handleClick}
       id={coord}
       key={coord}
-      style={style}
+      style={{
+        ...style,
+        outline: (state?.hl?.id === coord ? "solid 5px red" : ""),
+        outlineOffset: (state?.hl?.id === coord ? "-5px" : ""),
+      }}
     ></div>
   );
 }
