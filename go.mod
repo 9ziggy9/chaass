2b035baca17beacfb93eefcd312649c52c4d82ab
9ziggy9 <davidarogers@protonmail.com>
2023-03-07 12:47:33 -0800

    dotenv for go added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/9ziggy9/chaass
 
 go 1.20
 
-require github.com/gorilla/websocket v1.5.0 // indirect
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
-  "fmt"
+  "log"
+  "net/http"
+  "os"
+  "github.com/gorilla/websocket"
+  "github.com/joho/godotenv"
 )
 
+var upgrader = websocket.Upgrader{
+  ReadBufferSize: 1024,
+  WriteBufferSize: 1024,
+  // FIXME: Allowing all CORS requests
+  CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+      
+// ws message listener
+func reader(conn *websocket.Conn) {
+  for {
+    messageType, p, err := conn.ReadMessage()
+    if err != nil {
+      log.Println(err)
+      return
+    }
+
+    if err := conn.WriteMessage(messageType, p); err != nil {
+      log.Println(err)
+      return
+    }
+  }
+}
+
+// websocket endpoint
+func serveWs(w http.ResponseWriter, r *http.Request) {
+  
+}
+
 func main() {
-  fmt.Println("Hello, world!")
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+  port := os.Getenv("PORT")
+  log.Println("Opening socket on port "+port+"...")
+  log.Fatal(http.ListenAndServe(":"+port, nil))
 }
