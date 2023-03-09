b88c2e3670ab1e7d4c9ad61f63de0aa4017b56f2
9ziggy9 <davidarogers@protonmail.com>
2023-03-09 10:15:13 -0800

    omit .env and db

--- main.go
+++ main.go
@@ -9,8 +9,7 @@ import (
   "github.com/joho/godotenv"
   "gorm.io/driver/sqlite"
   "gorm.io/gorm"
-  "github.com/9ziggy9/chaass/models/game"
-  "github.com/9ziggy9/chaass/models/user"
+  "github.com/9ziggy9/chaass/models"
 )
 
 func loadEnv() (string, string) {
@@ -40,11 +39,11 @@ func main() {
   port, db := loadEnv() // Can exit(1)!
   dbConn := connectToDB(db) // Can exit(1)!
 
-  dbConn.AutoMigrate(&user.User{})
+  dbConn.AutoMigrate(&models.User{})
 
   // TODO: We are going to abstract this in api module
   // Create a new user
-  user := user.User{Name: "Ziggy"}
+  user := models.User{Name: "Ziggy"}
   result := dbConn.Create(&user)
   if result.Error != nil {
     log.Fatalf("Failed to create user: %v", result.Error)
@@ -52,7 +51,7 @@ func main() {
   }
 
   log.Println(port, db)
-  log.Println("Loaded module...", game.Hello())
+  log.Println("Loaded module...", models.Hello())
   log.Println("Listening on port "+port+"...")
   log.Fatal(http.ListenAndServe(":"+port, nil))
 }
--- models/game.go
+++ models/game.go
@@ -1,4 +1,4 @@
-package game
+package models
 
 func Hello() string {
   return "Hello, from game!"
