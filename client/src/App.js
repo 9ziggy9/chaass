bd1f783474b897c7e3a9917db76eae7df8439c83
9ziggy9 <davidarogers@protonmail.com>
2023-03-16 13:15:06 -0700

    modal implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,61 @@
 import Board from "./components/Board";
 import Chat from "./components/Chat";
 import {Switch, Route} from "react-router-dom";
+import ReactDOM from "react-dom";
+import {useState, createContext, useContext} from "react";
+
+function ModalPortal({children}) {
+  const {modal, handleModal, modalContent} = useContext(ModalContext);
+  return modal ? ReactDOM.createPortal(
+    <div id="modal-container">
+      <div
+        id="modal-overlay"
+        onClick={() => handleModal()}
+      ></div>
+      <div id="modal-content">
+        {modalContent}
+      </div>
+    </div>,
+    document.getElementById("portal-root")
+  ) : null;
+};
+
+function useModal() {
+  const [modal, setModal] = useState(false);
+  const [modalContent, setModalContent] = useState("Stuff in here.");
+  const handleModal = (content = false) => {
+    setModal(!modal);
+    if (content) {
+      setModalContent(content);
+    }
+  };
+  return {modal, handleModal, modalContent};
+}
+
+const ModalContext = createContext();
+const ModalProvider = ({children}) => {
+  const {modal, handleModal, modalContent} = useModal();
+  return (
+    <ModalContext.Provider value={{modal, handleModal, modalContent}}>
+      <ModalPortal/>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+const ChatModalBtn = () => {
+  const {handleModal} = useContext(ModalContext);
+  return (
+    <button onClick={() => handleModal(
+      <div>
+        <h2>HELLO WORLD</h2>
+        <p>HERE IS SOME STUFF</p>
+      </div>
+    )}>
+      OPEN SESAME
+    </button>
+  );
+};
 
 function App() {
   return (
@@ -8,6 +63,9 @@ function App() {
       <Switch>
         <Route path="/" exact>
 	  <Board />
+          <ModalProvider>
+            <ChatModalBtn />
+          </ModalProvider>
         </Route>
         <Route path="/chat-debug">
           <Chat />
